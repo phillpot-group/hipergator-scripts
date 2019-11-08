974bc675d93f210e6a619627d3e9b8c9c968403d
Seaton Ullberg <seatonullberg@gmail.com>
2019-11-07 22:24:57 -0500

    Update scancel_all.py

--- scancel_all.py
+++ scancel_all.py
@@ -3,9 +3,11 @@
 import os
 import subprocess
 
+USERNAME = "CUSTOM"  # change this to your hipergator username
+
 
 if __name__ == "__main__":
-    args = "squeue -u CUSTOM"           # replace CUSTOM with your hipergator username
+    args = "squeue -u {}".format(USERNAME)
     output = subprocess.getoutput(args) # get the text output of the prior command
     lines = output.split("\n")          # split the output into a list of lines
     for line in lines[1:]:              # ignore the first header line
